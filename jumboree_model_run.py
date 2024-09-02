d5c5ac7436863ce20d76d10fe08b091570d45b48
Katyayini Koneti <31547166+katyayini0583@users.noreply.github.com>
2024-09-02 21:27:37 +0530

    Update jumboree_model_run.py

--- jumboree_model_run.py
+++ jumboree_model_run.py
@@ -11,7 +11,7 @@ Uni_Rating = st.selectbox('Select University Rating',(1,2,3,4,5))
 SOP = 3
 LOR = 3
 CGPA = st.number_input('Enter CGPA between 6-10')
-Research = st.radio([0,1])
+Research = st.selectbox('If you have a research select 1 else 0',(0,1))
 input1 = [[GRE_Score,TOFEL_Score,Uni_Rating, SOP, LOR, CGPA, Research]]
 
 input = [[333, 118, 4, 4.5, 4.5, 9.65,1	]]
