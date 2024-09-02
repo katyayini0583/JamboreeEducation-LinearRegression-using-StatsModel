3b7ad43c501e8a38f9676283e210e692d1fc35cc
Katyayini Koneti <31547166+katyayini0583@users.noreply.github.com>
2024-09-02 21:38:15 +0530

    Update jumboree_model_run.py

--- jumboree_model_run.py
+++ jumboree_model_run.py
@@ -12,7 +12,7 @@ SOP = 3
 LOR = 3
 CGPA = st.number_input('Enter CGPA between 6-10')
 Research = st.selectbox('If you have a research select 1 else 0',(0,1))
-input1 = [GRE_Score,TOFEL_Score,Uni_Rating, SOP, LOR, CGPA, Research]
+input1 = [[GRE_Score,TOFEL_Score,Uni_Rating, SOP, LOR, CGPA, Research]]
 st.write(input1)
 #input = [[333, 118, 4, 4.5, 4.5, 9.65,1]]
 scaler = StandardScaler()
