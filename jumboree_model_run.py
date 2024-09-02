4bf886ef546171577a936aef552be57a1f388d3d
Katyayini Koneti <31547166+katyayini0583@users.noreply.github.com>
2024-09-02 21:05:38 +0530

    Update jumboree_model_run.py

--- jumboree_model_run.py
+++ jumboree_model_run.py
@@ -2,12 +2,15 @@ import pandas as pd
 import numpy as np
 import pickle 
 import streamlit as st
-
+from sklearn.preprocessing import StandardScaler
 st.header('Seat Prediction for Jumboree Education')
 
+input = [[333, 118, 4, 4.5, 4.5, 9.65,1	]]
+scaler = StandardScaler()
+input = scaler.fit_transform(input)
 with open('Jumbore_LinReg_model.pkl', 'rb') as file:
     loaded_model = pickle.load(file)
 
-chance = loaded_model.predict([[0.389986,	0.602418,	-0.098298,	0.126796,	0.564984,	0.415018,	0.895434]])
+chance = loaded_model.predict(input)
 
 st.write(f' Chance of getting seat in Jamboree is: {chance}')
