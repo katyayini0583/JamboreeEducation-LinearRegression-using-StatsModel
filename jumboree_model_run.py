99eacdefcadaad6e6dd9302f8e771249c6fb3d5d
Katyayini Koneti <31547166+katyayini0583@users.noreply.github.com>
2024-09-02 21:28:21 +0530

    Update jumboree_model_run.py

--- jumboree_model_run.py
+++ jumboree_model_run.py
@@ -14,9 +14,9 @@ CGPA = st.number_input('Enter CGPA between 6-10')
 Research = st.selectbox('If you have a research select 1 else 0',(0,1))
 input1 = [[GRE_Score,TOFEL_Score,Uni_Rating, SOP, LOR, CGPA, Research]]
 
-input = [[333, 118, 4, 4.5, 4.5, 9.65,1	]]
+#input = [[333, 118, 4, 4.5, 4.5, 9.65,1	]]
 scaler = StandardScaler()
-input = scaler.fit_transform(input)
+input = scaler.fit_transform(input1)
 with open('Jumbore_LinReg_model.pkl', 'rb') as file:
     loaded_model = pickle.load(file)
 
