86aeaf88972a971cae534831c616b1772b717e43
Katyayini Koneti <31547166+katyayini0583@users.noreply.github.com>
2024-09-02 20:58:48 +0530

    Update jumboree_model_run.py

--- jumboree_model_run.py
+++ jumboree_model_run.py
@@ -8,6 +8,6 @@ st.header('Seat Prediction for Jumboree Education')
 with open('Jumbore_LinReg_model.pkl', 'rb') as file:
     loaded_model = pickle.load(file)
 
-chance = loaded_model.predict(0.389986,	0.602418,	-0.098298,	0.126796,	0.564984,	0.415018,	0.895434)
+chance = loaded_model.predict([[0.389986,	0.602418,	-0.098298,	0.126796,	0.564984,	0.415018,	0.895434]])
 
 st.write(f' Chance of getting seat in Jamboree is: {chance}')
