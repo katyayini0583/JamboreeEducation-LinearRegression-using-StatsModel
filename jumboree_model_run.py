daa989aae509e662f447e2917a48af6659f2fbc8
Katyayini Koneti <31547166+katyayini0583@users.noreply.github.com>
2024-09-03 15:42:06 +0530

    Update jumboree_model_run.py

--- jumboree_model_run.py
+++ jumboree_model_run.py
@@ -3,7 +3,8 @@ import numpy as np
 import pickle 
 import streamlit as st
 from sklearn.preprocessing import StandardScaler
-st.header('Seat Prediction for Jumboree Education')
+
+st.header('Welcome to Jumboree Education!!.. Check your chance of admission')
 
 GRE_Score = st.slider('GRE Score',min_value= 260, max_value = 340)
 TOFEL_Score = st.slider('Tofel Score',min_value= 260, max_value = 340)
@@ -12,25 +13,25 @@ SOP = 3
 LOR = 3
 CGPA = st.number_input('Enter CGPA between 6-10')
 Research = st.selectbox('If you have a research select 1 else 0',(0,1))
+
+# step-1.Input from user
 input1 = [GRE_Score,TOFEL_Score,Uni_Rating, SOP, LOR, CGPA, Research]
 
+# step-2.Change input to 2-d array to transform
 input1_2d = np.array([input1])
-st.write(input1_2d.shape)
 
-
+#step-3. Load scaler from pickle
 with open('scaled_train.pkl', 'rb') as file:
     scaler = pickle.load(file) 
-#st.write(scaler)
-#st.write('new line')
 
-
-#main
+# Step-4. Transform the input with scaler
 scaled_input = scaler.transform(input1_2d)
-#st.write(scaled_input)
 
+# Step-5:Load model from pickle
 with open('Jumbore_LinReg_model.pkl', 'rb') as file:
     loaded_model = pickle.load(file)
 
+# Step-6. Predict using model
 chance = loaded_model.predict(scaled_input)
 if st.button('Submit'):
     st.write(f' Chance of getting seat in Jamboree is: {chance[0]}')
